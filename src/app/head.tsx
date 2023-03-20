9de2398b5c1f500abac8493cbd75be60fd01f3b0
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2023-03-20 12:02:38 -0700

    Test

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -20,16 +20,19 @@ export default function Head() {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
+      <meta name="theme-color" content="#000000"></meta>
       <link rel="manifest" href="/site.webmanifest" />
       <meta name="mobile-web-app-capable" content="yes" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="description" content="All-in-One Fitness Solution" />
+      <meta
+        name="description"
+        content="Study from over 2250 performance indicators"
+      />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1., maximum-scale=1"
       ></meta>
-      <meta name="theme-color" content="#000000"></meta>
     </>
   );
 }
